37f0baf98bf334ca522af1ea2b73dec89c9c6dcd
WaScript3r <wascript3r@gmail.com>
2022-05-05 19:59:16 +0300

    added different resizing strategies

--- resizer.go
+++ resizer.go
@@ -1,7 +1,26 @@
 package gopool
 
 import (
+	"runtime"
 	"sync"
+)
+
+var maxProcs = runtime.GOMAXPROCS(0)
+
+// Preset pool resizing strategies
+var (
+	// Eager maximizes responsiveness at the expense of higher resource usage,
+	// which can reduce throughput under certain conditions.
+	// This strategy is meant for worker pools that will operate at a small percentage of their capacity
+	// most of the time and may occasionally receive bursts of tasks. It's the default strategy.
+	Eager = func() ResizingStrategy { return NewRatedResizer(1) }
+	// Balanced tries to find a balance between responsiveness and throughput.
+	// It's suitable for general purpose worker pools or those
+	// that will operate close to 50% of their capacity most of the time.
+	Balanced = func() ResizingStrategy { return NewRatedResizer(maxProcs / 2) }
+	// Lazy maximizes throughput at the expense of responsiveness.
+	// This strategy is meant for worker pools that will operate close to their max. capacity most of the time.
+	Lazy = func() ResizingStrategy { return NewRatedResizer(maxProcs) }
 )
 
 // ratedResizer implements a rated resizing strategy
