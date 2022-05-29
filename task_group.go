75e30ec280038f2a521751c5edc062c4cd1441a5
WaScript3r <wascript3r@gmail.com>
2022-05-29 17:36:16 +0300

    fixed task group bug

--- task_group.go
+++ task_group.go
@@ -11,10 +11,17 @@ type TaskGroup struct {
 // Schedule adds a task to this group and sends it to the worker pool to be executed
 func (g *TaskGroup) Schedule(task func()) error {
 	g.wg.Add(1)
-	return g.pool.Schedule(func() {
+
+	err := g.pool.Schedule(func() {
 		defer g.wg.Done()
 		task()
 	})
+	if err != nil {
+		g.wg.Done()
+		return err
+	}
+
+	return nil
 }
 
 // Wait waits until all the tasks in this group have completed
